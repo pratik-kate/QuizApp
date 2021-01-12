685439d68d558c00303bcc04b398344456caaece
pratikkate3 <pratikrkate@gmail.com>
2021-01-12 16:54:56 +0530

    initial commit

---
+++ app/src/main/java/com/supertridents/quizz/MainActivity.java
@@ -0,0 +1,34 @@
+package com.supertridents.quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView setting,gifts,game;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        setting = findViewById(R.id.settings);
+        gifts = findViewById(R.id.gifts);
+        game = findViewById(R.id.startGame);
+
+        game.setOnClickListener(v -> {
+            Intent game = new Intent(MainActivity.this,GameActivity.class);
+            startActivity(game);
+        });
+        setting.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/supertridents/quizz/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.supertridents.quizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().hide();
+    }
+}
