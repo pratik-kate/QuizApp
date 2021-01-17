1f1ff730cc06466b9a3cb008fdfc5ca188c2a677
pratikkate3 <pratikrkate@gmail.com>
2021-01-17 11:36:56 +0530

    Levels Layout

--- app/src/main/java/com/supertridents/quizz/MainActivity.java
+++ app/src/main/java/com/supertridents/quizz/MainActivity.java
@@ -12,24 +12,29 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView setting,gifts,game;
+    TextView setting,exit,game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        //setting = findViewById(R.id.settings);
-        //gifts = findViewById(R.id.gifts);
+        setting = findViewById(R.id.options);
+        exit = findViewById(R.id.exit);
         game = findViewById(R.id.startgame);
 
         game.setOnClickListener(v -> {
             Intent game = new Intent(MainActivity.this,QuestionActivity.class);
             startActivity(game);
         });
-//        setting.setOnClickListener(v -> {
-//            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-//            startActivity(intent);
-//        });
+        setting.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        exit.setOnClickListener( v ->{
+            super.onBackPressed();
+        });
     }
+
 }
