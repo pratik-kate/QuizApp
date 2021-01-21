bf897d9509db372a2b65e84c96f8b6ad7b044fd2
pratikkate3 <pratikrkate@gmail.com>
2021-01-21 17:15:44 +0530

    Splash Updated

--- app/src/main/java/com/supertridents/learn/digital/marketing/quiz/MainActivity.java
+++ app/src/main/java/com/supertridents/learn/digital/marketing/quiz/MainActivity.java
@@ -1,10 +1,13 @@
 package com.supertridents.learn.digital.marketing.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     public static int currency;
     public static int coins;
     TextView setting,exit,game;
+    CardView profile,settings,shop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         setting = findViewById(R.id.options);
         exit = findViewById(R.id.exit);
         game = findViewById(R.id.startgame);
+        profile = findViewById(R.id.cardProfile);
+        settings = findViewById(R.id.cardSettings);
+        shop = findViewById(R.id.cardShop);
 
         game.setOnClickListener(v -> {
             Intent game = new Intent(MainActivity.this,QuestionActivity.class);
@@ -34,7 +41,11 @@ public class MainActivity extends AppCompatActivity {
         });
 
         exit.setOnClickListener( v ->{
-            super.onBackPressed();
+            exitByBackKey();
+        });
+
+        settings.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this,SettingsActivity.class));
         });
 
         SharedPreferences preferences = getSharedPreferences(LEVEL,MODE_PRIVATE);
@@ -44,4 +55,26 @@ public class MainActivity extends AppCompatActivity {
         coins.setText(String.valueOf(coin));
     }
 
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            exitByBackKey();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    protected void exitByBackKey() {
+
+        AlertDialog alertbox = new AlertDialog.Builder(this)
+                .setMessage("Do you want to quit the game?")
+                .setPositiveButton("Yes", (arg0, arg1) -> {
+                    Intent a = new Intent(Intent.ACTION_MAIN);
+                    a.addCategory(Intent.CATEGORY_HOME);
+                    a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(a);
+                    finish();
+                })
+                .setNegativeButton("No", (arg0, arg1) -> arg0.dismiss())
+                .show();
+    }
 }
